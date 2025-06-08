fd81780430cbe2355ddee27ac44d23de1ae43b86
WilMaz00602 <51935355+WilMaz00602@users.noreply.github.com>
2025-06-07 21:04:55 -0300

    Added Content to About Page

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -1,5 +1,10 @@
 import styles from "../resources/style/about.scss";
 
+import FrontendDevelopmentIcon from "../resources/images/frontend_development";
+import SoftwareTestingIcon from "../resources/images/software_testing";
+import EmbeddedSystemsIcon from "../resources/images/embedded_systems";
+import AlgorithmDesignIcon from "../resources/images/algorithms";
+
 export default function About() {
     return (
         <article>
@@ -9,29 +14,43 @@ export default function About() {
             <hr className="accent1" />
             <section>
                 <p>
-                    I am a software engineering student at the University of New Brunswick, with 
+                    I am a software engineering student at the University of New Brunswick, with a passion for leveraging technology to automate and optimize tasks through functional interfaces. 
+                    I enjoy solving complex problems and learning new technologies as it makes me more versatile and refines my core development principles. 
+                    I am always eager to take on new challenges and grow as a developer in my path to create impactful products that enable everyday people to get more done.
                 </p>
             </section>
             <section>
                 <h2>What I Do</h2>
                 <div className="work-row">
                     <div className="work-item">
-                        <h3>Frontend Development</h3>
-                        <p>Professional experience developing frontend applications designed for web and desktop environments</p>
+                        <FrontendDevelopmentIcon className="work-image" />
+                        <div>
+                            <h3>Frontend Development</h3>
+                            <p>Professional experience developing frontend applications designed for web and desktop environments.</p>
+                        </div>
                     </div>
                     <div className="work-item">
-                        <h3>Software Testing</h3>
-                        <p>Automated and manual testing experience for web and desktop applications</p>
+                        <SoftwareTestingIcon className="work-image" />
+                        <div>
+                            <h3>Software Testing</h3>
+                            <p>Automated and manual testing experience for web and desktop applications. I have a talent for breaking things!</p>
+                        </div>
                     </div>
                 </div>
                 <div className="work-row">
                     <div className="work-item">
-                        <h3>Embedded Systems</h3>
-                        <p>Practice in electronics design and embedded systems development and keen on doing more</p>
+                        <EmbeddedSystemsIcon className="work-image" />
+                        <div>
+                            <h3>Embedded Systems</h3>
+                            <p>Practice in electronics design and embedded systems development and keen on doing more.</p>
+                        </div>
                     </div>
                     <div className="work-item">
-                        <h3>Algorithm Design</h3>
-                        <p>Experience in writing data processing and algebraic algorithms to develop custom backend solutions</p>
+                        <AlgorithmDesignIcon className="work-image" />
+                        <div>
+                            <h3>Algorithm Design</h3>
+                            <p>Experience in writing data processing and algebraic algorithms to develop custom backend solutions.</p>
+                        </div>
                     </div>
                 </div>
             </section>
